f046aaa85ff709ed70cb76c64abfabf0a48ef624
Xin Wang <xwang@d-matrix.ai>
2021-05-11 23:39:04 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="compression",
+    version="0.0.1.dev",
+    description="d-MATRiX neural net compression",
+    author="Xin Wang",
+    author_email="xwang@d-matrix.ai",
+    license="MIT",
+    packages=find_packages('src', exclude=('tests', 'docs', 'experiments', 'sandbox')),
+    package_dir={'': 'src'},
+    install_requires=(
+        'torch',
+        'torchvision',
+        'transformers',
+        'numpy',
+        'tqdm',
+        'python-dotenv',
+    ),
+    python_requires='>=3.6')
