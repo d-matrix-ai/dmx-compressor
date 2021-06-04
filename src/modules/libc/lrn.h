0092f13cefdaf2395fab17bddffc6444d1cd1092
Xin Wang <xwang@d-matrix.ai>
2021-06-04 07:22:34 -0700

    Removed c sources from master

--- src/modules/libc/lrn.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-#include "mtlib.h"
-
-#define vecblk_len 1024
-
-double x32b[vecblk_len];
--- src/modules/libc/softmax.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-#include "mtlib.h"
-
-#define vecblk_len 384
-#define vecblk_row 384
-
