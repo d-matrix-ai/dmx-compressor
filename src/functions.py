7d64b36d5602ea9c5752f488262f30263e2048c9
Ilya Lyubomirsky <ilyubnn@d-matrix.ai>
2021-07-28 09:35:26 -0700

    removed with statement in poly2softmax and removed n which is no longer used, added comments on function description

--- src/functions.py
+++ src/functions.py
@@ -9,43 +9,44 @@ __ALL__ = [
 ]
 
 def poly2softmax(x, dim=-1):
-    with torch.no_grad():
-        n = x.shape[dim]
-        eps=1.e-30  #small number to avoid dividing by zero 
-        ln2 = 0.69315  # log(2)
-        invln2 = 1.4427  # 1 / log(2)
-        scale = 14  # for exp() poly approximation assume 16bit integer arithmetic 
+#This function computes Softmax using a range
+#reduction technique and second order polynomial approximatin
+#for exp(r), where r is in reduced range 
+    eps=1.e-30  #small number to avoid dividing by zero 
+    ln2 = 0.69315  # log(2)
+    invln2 = 1.4427  # 1 / log(2)
+    scale = 14  # for exp() poly approximation assume 16bit integer arithmetic 
                     # with 14 bit fractional part
-        c0int = 2**scale  # poly coefficient c0 = 1
-        c1int = round(1.0151 * 2**scale)  # poly coefficient c1
-        # note poly coefficient c2 is 0.5 
+    c0int = 2**scale  # poly coefficient c0 = 1
+    c1int = round(1.0151 * 2**scale)  # poly coefficient c1
+    # note poly coefficient c2 is 0.5 
 
-        # y = torch.zeros_like(x) # initialize output vector
+    # y = torch.zeros_like(x) # initialize output vector
 
-        # range reduction to range -log(2)/2 < r < log(2)/2
-        k = torch.round(x * invln2)
-        r = x - k * ln2
+    # range reduction to range -log(2)/2 < r < log(2)/2
+    k = torch.round(x * invln2)
+    r = x - k * ln2
 
-        # compute exp(r) emulating fixed point arithmetic 
-        rint = torch.round(r * 2**scale)
-        # r2int = torch.round(rint * rint * 2**(-scale))
-        # mult_add1 = torch.round(c0int + torch.round(c1int * rint * 2**(-scale)))
-        # mult_add2 = torch.round(mult_add1 + 0.5 * r2int)   
-        mult_add1 = torch.round(c1int + 0.5*rint)
-        mult_add2 = torch.round(c0int + torch.round(mult_add1*rint*2**(-scale)))
+    # compute exp(r) emulating fixed point arithmetic 
+    rint = torch.round(r * 2**scale)
+    # r2int = torch.round(rint * rint * 2**(-scale))
+    # mult_add1 = torch.round(c0int + torch.round(c1int * rint * 2**(-scale)))
+    # mult_add2 = torch.round(mult_add1 + 0.5 * r2int)   
+    mult_add1 = torch.round(c1int + 0.5*rint)
+    mult_add2 = torch.round(c0int + torch.round(mult_add1*rint*2**(-scale)))
 
-        y = mult_add2 * 2**(-scale)
+    y = mult_add2 * 2**(-scale)
 
-        kmax,_ = torch.max(k,dim=dim,keepdim=True) #find max k along softmax dim.
-        # shift by k-kmax bits for final result 
-        # kmax,_ = torch.max(k,dim=-1)
-        # kmax = torch.unsqueeze(kmax,dim=-1)
-        y *= 2**(k-kmax)
+    kmax,_ = torch.max(k,dim=dim,keepdim=True) #find max k along softmax dim.
+    # shift by k-kmax bits for final result 
+    # kmax,_ = torch.max(k,dim=-1)
+    # kmax = torch.unsqueeze(kmax,dim=-1)
+    y *= 2**(k-kmax)
 
-        # compute softmax (note will need to use 32 bits for sum in HW)
-        sum_exp = torch.sum(y, dim=dim, keepdim=True)
-        y /= (sum_exp + eps)
-        # y = torch.round(y * 256) / 256
+    # compute softmax (note will need to use 32 bits for sum in HW)
+    sum_exp = torch.sum(y, dim=dim, keepdim=True)
+    y /= (sum_exp + eps)
+    # y = torch.round(y * 256) / 256
         
     return y
 
