50f57f69f0b7eed1327a11d12c68398b33b30e18
Xin Wang <xwang@d-matrix.ai>
2021-07-29 09:33:20 -0700

    Cleaned up data.py

--- src/data.py
+++ src/data.py
@@ -5,21 +5,22 @@ from functools import partial
 
 
 class MNIST():
-    def __init__(self,
-                 data_dir='~/data/mnist',
-                 train_batch_size=64,
-                 test_batch_size=1000,
-                 cuda=False,
-                 num_workers=1,
-                 shuffle=True
-                ):
+    def __init__(
+            self,
+            data_dir='~/data/mnist',
+            train_batch_size=64,
+            test_batch_size=1000,
+            cuda=False,
+            num_workers=1,
+            shuffle=True
+        ):
         cuda_conf = {
             'num_workers': num_workers,
             'pin_memory': True,
         } if cuda else {}
         transform = tv.transforms.Compose([
             tv.transforms.ToTensor(),
-            tv.transforms.Normalize((0.1307, ), (0.3081, ))
+            tv.transforms.Normalize((0.1307,), (0.3081,))
         ])
         self.train = torch.utils.data.DataLoader(
             tv.datasets.MNIST(
@@ -35,22 +36,24 @@ class MNIST():
 
 
 class CIFAR():
-    def __init__(self,
-                 num_classes=10,
-                 data_dir='~/data/cifar',
-                 train_batch_size=64,
-                 test_batch_size=64,
-                 cuda=False,
-                 num_workers=4,
-                 shuffle=True):
+    def __init__(
+            self,
+            num_classes=10,
+            data_dir='~/data/cifar',
+            train_batch_size=64,
+            test_batch_size=64,
+            cuda=False,
+            num_workers=4,
+            shuffle=True
+        ):
         gpu_conf = {
             'num_workers': num_workers,
             'pin_memory': True
         } if cuda else {}
         normalize = tv.transforms.Normalize(
-            [0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010]
+            (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
         ) if num_classes==10 else tv.transforms.Normalize(
-            [0.507, 0.4865, 0.4409], [0.2673, 0.2564, 0.2761]
+            (0.5070, 0.4865, 0.4409), (0.2673, 0.2564, 0.2761)
         )
         transform_train = tv.transforms.Compose([
             tv.transforms.Pad(4, padding_mode='reflect'),
@@ -83,19 +86,22 @@ CIFAR100 = partial(CIFAR, num_classes=100)
 
 
 class I1K():
-    def __init__(self,
-                 data_dir='~/data/imagenet',
-                 cuda=False,
-                 num_workers=8,
-                 train_batch_size=64,
-                 test_batch_size=500,
-                 shuffle=True):
+    def __init__(
+            self,
+            data_dir='~/data/imagenet',
+            cuda=False,
+            num_workers=8,
+            train_batch_size=64,
+            test_batch_size=500,
+            shuffle=True
+        ):
         gpu_conf = {
             'num_workers': num_workers,
             'pin_memory': True
         } if cuda else {}
         normalize = tv.transforms.Normalize(
-            mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+            (0.485, 0.456, 0.406), (0.229, 0.224, 0.225),
+        )
         self.train = torch.utils.data.DataLoader(
             tv.datasets.ImageFolder(
                 data_dir + '/train',
