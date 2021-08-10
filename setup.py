c378b4cba5600fd4a50a284f9e3a23fac6e8cd1c
Xin Wang <xwang@d-matrix.ai>
2021-08-10 10:55:04 -0700

    Modified setup.py to include dmir.proto compilation

--- setup.py
+++ setup.py
@@ -1,4 +1,26 @@
+import os
 from setuptools import find_packages, setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from subprocess import check_call
+
+
+DMIR_PROTO_DIR = "./src/utils/"
+DMIR_PROTO_FILE = "dmir.proto"
+
+class PostDevelopCommand(develop):
+    """Post-installation for development mode."""
+    def run(self):
+        develop.run(self)
+        if os.path.exists(os.path.join(DMIR_PROTO_DIR, DMIR_PROTO_FILE)):
+            os.system(f"protoc -I={DMIR_PROTO_DIR} --python_out={DMIR_PROTO_DIR} {os.path.join(DMIR_PROTO_DIR, DMIR_PROTO_FILE)}")
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        if os.path.exists(os.path.join(DMIR_PROTO_DIR, DMIR_PROTO_FILE)):
+            os.system(f"protoc -I={DMIR_PROTO_DIR} --python_out={DMIR_PROTO_DIR} {os.path.join(DMIR_PROTO_DIR, DMIR_PROTO_FILE)}")
 
 setup(
     name="compression",
@@ -7,22 +29,27 @@ setup(
     author="Xin Wang",
     author_email="xwang@d-matrix.ai",
     license="MIT",
-    packages=find_packages('src', exclude=('tests', 'docs', 'experiments', 'sandbox')),
-    package_dir={'': 'src'},
+    packages=find_packages("src", exclude=("tests", "docs", "experiments", "sandbox")),
+    package_dir={"": "src"},
     install_requires=(
-        'torch==1.7.1',
-        'torchvision',
-        'transformers @ git+ssh://git@git.d-matrix.ai/xin/transformers.git@dm-refactor',
-        'datasets',
-        'numpy',
-        'scipy',
-        'sklearn',
-        'qtorch>=0.3',
-        'pyyaml',
-        'tqdm',
-        'parse',
-        'ninja',
-        'python-dotenv',
-        'pytest',
+        "torch==1.7.1",
+        "torchvision",
+        "transformers @ git+ssh://git@git.d-matrix.ai/xin/transformers.git@dm-refactor",
+        "datasets",
+        "numpy",
+        "scipy",
+        "sklearn",
+        "qtorch>=0.3",
+        "pyyaml",
+        "tqdm",
+        "parse",
+        "ninja",
+        "python-dotenv",
+        "pytest",
     ),
-    python_requires='>=3.6')
+    python_requires=">=3.6",
+    cmdclass={
+        "develop": PostDevelopCommand,
+        "install": PostInstallCommand,
+    },
+)
