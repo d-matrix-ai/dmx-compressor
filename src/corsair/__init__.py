c9b2d0dc1ef475d7bcea4a47bc38b930470328c1
Xin Wang <xwang@d-matrix.ai>
2021-08-24 11:20:16 -0700

    Added refactored corsair.nn

---
+++ src/corsair/__init__.py
@@ -0,0 +1,24 @@
+import torch
+from numerical import CastTo
+from sparse import Sparsify
+from approximate import Approximate
+from .nn import *
+
+
+# add new torch.nn modules for corsair
+torch.nn.CastTo = CastTo
+torch.nn.Sparsify = Sparsify
+torch.nn.Approximate = Approximate
+
+# overload existing torch.nn modules for corsair
+torch.nn.Linear = Linear
+torch.nn.Conv2d = Conv2d
+torch.nn.AdaptiveAvgPool2d = AdaptiveAvgPool2d
+torch.nn.MaxPool2d = MaxPool2d
+torch.nn.BatchNorm2d = BatchNorm2d
+torch.nn.LayerNorm = LayerNorm
+torch.nn.Dropout = Dropout
+torch.nn.Softmax = Softmax
+torch.nn.ReLU = ReLU
+torch.nn.ReLU6 = ReLU6
+torch.nn.Tanh = Tanh
