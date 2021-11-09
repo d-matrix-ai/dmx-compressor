8a5040b361c6cd06cf823c20cc7a8ef6dd15ee08
Xin Wang <xwang@d-matrix.ai>
2021-11-09 19:20:33 +0000

    Revised run_cifar

--- scripts/run_cifar.py
+++ scripts/run_cifar.py
@@ -6,8 +6,8 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 import torch.onnx
-from data import CIFAR10, CIFAR100
-from utils import load_config_file, mask2braille
+from mltools.data import CIFAR10, CIFAR100
+from mltools.utils import load_config_file, mask2braille
 
 
 def parse_args():
@@ -90,6 +90,7 @@ MODEL_LIST = [
 ]
 pb_wrap = lambda it: tqdm(it, leave=False, dynamic_ncols=True)
 
+
 def evaluate(model, ds):
     print(f"Evaluating {args.model} on {args.dataset}...")
     model.eval()
@@ -106,18 +107,21 @@ def evaluate(model, ds):
                 correct += (predicted == labels).sum().item()
     print(f"\tAccuracy : {100. * correct / total :.2f}%")
 
+
 def train(model, ds, epochs):
     print(f"Training {args.model} on {args.dataset}...")
     criterion = torch.nn.CrossEntropyLoss().to(device)
     optimizer = torch.optim.SGD(
-        model.parameters(), 
-        lr=args.lr, 
+        model.parameters(),
+        lr=args.lr,
         momentum=0.9,
         nesterov=True,
         weight_decay=1e-4,
     )
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs*len(ds))
-    for epoch in range(1, epochs+1):
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, T_max=epochs * len(ds)
+    )
+    for epoch in range(1, epochs + 1):
         model.train()
         with pb_wrap(ds) as loader:
             loader.set_description(f"Epoch {epoch}")
@@ -128,17 +132,22 @@ def train(model, ds, epochs):
                 loss = criterion(output, labels)
                 loss.backward()
                 optimizer.step()
-                loader.set_postfix(dict(
-                    lr=f"{scheduler.get_last_lr()[0] :.8f}",
-                    loss="\33[91m{:6.4f}\033[0m".format(loss),
-                ))
+                loader.set_postfix(
+                    dict(
+                        lr=f"{scheduler.get_last_lr()[0] :.8f}",
+                        loss="\33[91m{:6.4f}\033[0m".format(loss),
+                    )
+                )
                 scheduler.step()
         evaluate(model, dataset.test)
     return model
 
+
 if __name__ == "__main__":
 
-    import corsair
+    from mltools import corsair
+
+    corsair.aware()  # pytorch is now extended to be "corsair-aware"
 
     # load dataset
     dataset = eval(args.dataset.upper())(
@@ -154,25 +163,22 @@ if __name__ == "__main__":
     assert (
         args.model in MODEL_LIST
     ), f"unrecognized model {args.model}, supported models: \n{MODEL_LIST}"
-    model = torch.hub.load(
-        "chenyaofo/pytorch-cifar-models",
-        args.dataset + "_" + args.model,
-        pretrained=True,
+    model = corsair.Model(
+        torch.hub.load(
+            "chenyaofo/pytorch-cifar-models",
+            args.dataset + "_" + args.model,
+            pretrained=True,
+        )
     ).to(device)
 
     config = load_config_file(args.config)
+    # evaluate model
+    evaluate(model, dataset.test)
+    # transform
     model.transform(config)
     # evaluate model
     evaluate(model, dataset.test)
-    
+    # more training
     model = train(model, dataset.train, args.epochs)
-    # for nnz in [7, 6, 5, 4]:
-    #     config["transformation_rules"][0]["config"]["weight_sparseness"] = f"BTOPK{{{nnz}:8,1}}"
-    #     model.transform(config)
-    #     model = train(model, dataset.train, 10)
-    # for nnz in [3, 2]:
-    #     config["transformation_rules"][0]["config"]["weight_sparseness"] = f"BTOPK{{{nnz}:8,1}}"
-    #     model.transform(config)
-    #     model = train(model, dataset.train, 25)
     # evaluate model
     evaluate(model, dataset.test)
