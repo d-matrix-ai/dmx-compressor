3e0bb4eb38baa0be69ea39216f28c3c29ac06185
Tristan Webb <twebb@d-matrix.ai>
2022-06-24 12:15:58 -0700

    Additional test with "fake casts"

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn.functional as F
 import torch.nn as nn
 from mltools import corsair
-from mltools.corsair.transform import CorsairTransform
+from mltools.corsair.transform import cast_input_output_transform
 import inspect
 import ipdb
 
@@ -16,30 +16,13 @@ torch.manual_seed(RANDOM_SEED)
 
 def test_corsair_transform():
     net = Net()
-    net = corsair.Model(net)
 
     cnet = CorsairNet()
-    cnet = corsair.Model(cnet)
 
-    gm = torch.fx.symbolic_trace(net.body)
-    cgm = torch.fx.symbolic_trace(cnet.body)
-    # ipdb.set_trace()
-    for i in gm.graph.nodes:
-        if i.target == 'input':
-            gm.graph.inserting_after(i)
-            gm.graph.create_node('call_method','clone',args=(i,))
-        elif i.target =='output':
-            gm.graph.inserting_before(i)
-            prev=gm.graph.create_node('call_method','clone',args=(prev,))
-            i.args = (prev,)
-        else:
-            if len(i.args)!=0:
-                i.args = (prev,)    
-        prev = i
-    
-    # transformed : torch.nn.Module = CorsairTransform(gm).transform()
-    # ipdb.set_trace()
-    gm.recompile()
+    cgm = torch.fx.symbolic_trace(cnet)
+
+    gm = cast_input_output_transform(net)
+
     assert(cgm.code == gm.code)
 
 class Net(torch.nn.Module):
@@ -54,12 +37,10 @@ class CorsairNet(torch.nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.linear = nn.Linear(64, 64)
-        #self.linear = corsair.nn.Linear(64,64)
 
     def forward(self, input):
-        # return self.linear(input)
         input_1 = input
-        clone = input_1.clone();  input_1 = None
-        linear = self.linear(clone);  clone = None
-        clone_1 = linear.clone();  linear = None
+        clone = input_1.clone();
+        linear = self.linear(clone);
+        clone_1 = linear.clone();
         return clone_1
