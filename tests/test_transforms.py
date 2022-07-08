5c892c71797fea1443e7420149821e8d20c008d0
Tristan Webb <twebb@d-matrix.ai>
2022-07-07 21:52:49 -0700

    formatting

--- tests/test_transforms.py
+++ tests/test_transforms.py
@@ -17,23 +17,24 @@ RANDOM_SEED = 0
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 torch.manual_seed(RANDOM_SEED)
 
-def checkTransform(gm:nn.Module) -> bool:
-    if not isinstance(gm,fx.graph_module.GraphModule):
+
+def checkTransform(gm: nn.Module) -> bool:
+    if not isinstance(gm, fx.graph_module.GraphModule):
         return True
     nodeList = []
     for i in gm.graph.nodes:
         nodeList.append(i)
     for i in nodeList:
-        if i.op=="placeholder":
-            if i.next.target!='input_cast':
+        if i.op == "placeholder":
+            if i.next.target != "input_cast":
                 return False
-        elif i.op == 'output':
-            if i.prev.target!='output_cast':
+        elif i.op == "output":
+            if i.prev.target != "output_cast":
                 return False
-        elif i.op =='get_attr':
-            if i.next.target!='weight_cast':
+        elif i.op == "get_attr":
+            if i.next.target != "weight_cast":
                 return False
-        elif i.op=="call_module":
+        elif i.op == "call_module":
             if not checkTransform(gm.get_submodule(i.target)):
                 return False
     return True
@@ -43,133 +44,155 @@ def test_corsair_transform():
     net = Net()
     cnet = CorsairNet()
     gm_before = fx.symbolic_trace(net)
-    assert not checkTransform(gm_before),True
+    assert not checkTransform(gm_before), True
     gm = cast_input_output_transform(net)
-    input = torch.rand(1,64)
+    input = torch.rand(1, 64)
 
     # Initialize bias and weight
-    weight = torch.rand(64,64)
-    bias = torch.zeros(1,64)
-    cnet.linear.bias.data  = bias
+    weight = torch.rand(64, 64)
+    bias = torch.zeros(1, 64)
+    cnet.linear.bias.data = bias
     gm.linear.bias.data = bias
     cnet.linear.weight.data = weight
     gm.linear.weight.data = weight
 
-    coutput =cnet(input)
+    coutput = cnet(input)
     output = gm(input)
-    assert (coutput-output).abs().sum()==0,True
-    assert checkTransform(gm),True
+    assert (coutput - output).abs().sum() == 0, True
+    assert checkTransform(gm), True
 
 
 class downcast(nn.Module):
     def __init__(self, format="SAME", dump_to=None):
         super().__init__()
+
     def forward(self, x):
         return x
+
 
 class upcast(nn.Module):
     def __init__(self, format="SAME", dump_to=None):
         super().__init__()
+
     def forward(self, x):
         return x
+
 
 class weightCast(nn.Module):
     def __init__(self, format="SAME", dump_to=None):
         super().__init__()
+
     def forward(self, x):
         return x
 
+
 def test_fakecast_transform():
     net = Net()
-    gm = cast_input_output_transform(net, downcast(), upcast(),weightCast())
-    assert checkTransform(gm),True
+    gm = cast_input_output_transform(net, downcast(), upcast(), weightCast())
+    assert checkTransform(gm), True
+
 
 def test_lenet_1hid_corsair_transform():
-    net = LeNet([10,10])
+    net = LeNet([10, 10])
     gm = cast_input_output_transform(net)
-    assert checkTransform(gm),True 
+    assert checkTransform(gm), True
+
 
 def test_lenet_5hid_corsiar_transform():
-    net = LeNet([10,10,10,10,10,10])
+    net = LeNet([10, 10, 10, 10, 10, 10])
     gm = cast_input_output_transform(net)
-    assert checkTransform(gm),True 
+    assert checkTransform(gm), True
+
 
 def test_lenet_1hid_fakecast_transform():
-    net = LeNet([10,10])
-    gm = cast_input_output_transform(net,downcast(),upcast())
-    assert checkTransform(gm),True 
+    net = LeNet([10, 10])
+    gm = cast_input_output_transform(net, downcast(), upcast())
+    assert checkTransform(gm), True
+
 
 def test_lenet_5hid_fakecast_transform():
-    net = LeNet([10,10,10,10,10,10])
-    gm = cast_input_output_transform(net,downcast(),upcast())
-    assert checkTransform(gm),True 
+    net = LeNet([10, 10, 10, 10, 10, 10])
+    gm = cast_input_output_transform(net, downcast(), upcast())
+    assert checkTransform(gm), True
+
 
 def test_conv2D_fakecast_transform():
-    net = torch.nn.Conv2d(16,8,2)
-    cnet = corsair.nn.Conv2d(16,8,2)
-    input = torch.rand(1,16,16,16)
-    weight = torch.rand(8,16,2,2)
+    net = torch.nn.Conv2d(16, 8, 2)
+    cnet = corsair.nn.Conv2d(16, 8, 2)
+    input = torch.rand(1, 16, 16, 16)
+    weight = torch.rand(8, 16, 2, 2)
     bias = torch.rand(8)
     net.weight.data = weight
     cnet.weight.data = weight
     net.bias.data = bias
     cnet.bias.data = bias
     coutput = cnet(input)
-    
-    gm = cast_input_output_transform(net,downcast(),upcast())
+
+    gm = cast_input_output_transform(net, downcast(), upcast())
     output = gm(input)
-    assert (coutput-output).abs().sum()==0,True
-    assert checkTransform(gm),True 
+    assert (coutput - output).abs().sum() == 0, True
+    assert checkTransform(gm), True
+
 
 def test_Dropout_transform():
     net = torch.nn.Dropout()
     gm = cast_input_output_transform(net)
-    assert checkTransform(gm),True 
+    assert checkTransform(gm), True
+
 
 def test_AdaptiveAvgPool2d_transform():
     net = torch.nn.AdaptiveAvgPool2d(16)
-    gm = cast_input_output_transform(net) 
+    gm = cast_input_output_transform(net)
     assert checkTransform(gm), True
+
 
 def test_AvgPool2d_transform():
     net = torch.nn.AvgPool2d(8)
-    gm = cast_input_output_transform(net) 
+    gm = cast_input_output_transform(net)
     assert checkTransform(gm), True
+
 
 def test_MaxPool2d_transform():
     net = torch.nn.MaxPool2d(16)
     gm = cast_input_output_transform(net)
     assert checkTransform(gm), True
 
+
 def test_Softmax_transform():
     net = torch.nn.Softmax()
     gm = cast_input_output_transform(net)
     assert checkTransform(gm), True
 
+
 def test_LayerNorm_transform():
-    net = torch.nn.LayerNorm((1,64))
+    net = torch.nn.LayerNorm((1, 64))
     gm = cast_input_output_transform(net)
     assert checkTransform(gm), True
+
 
 def test_ReLU_transform():
     net = torch.nn.ReLU()
     gm = cast_input_output_transform(net)
     assert checkTransform(gm)
 
+
 def test_ReLU6_transform():
     net = torch.nn.ReLU6()
     gm = cast_input_output_transform(net)
     assert checkTransform(gm), True
+
 
 def test_Tanh_transform():
     net = torch.nn.Tanh()
     gm = cast_input_output_transform(net)
     assert checkTransform(gm), True
 
+
 def test_GELU_transform():
     net = torch.nn.GELU()
     gm = cast_input_output_transform(net)
     assert checkTransform(gm), True
+
 
 class Net(torch.nn.Module):
     def __init__(self) -> None:
@@ -179,6 +202,7 @@ class Net(torch.nn.Module):
     def forward(self, input):
         return self.linear(input)
 
+
 class CorsairNet(torch.nn.Module):
     def __init__(self) -> None:
         super().__init__()
@@ -186,4 +210,3 @@ class CorsairNet(torch.nn.Module):
 
     def forward(self, input):
         return self.linear(input)
-
