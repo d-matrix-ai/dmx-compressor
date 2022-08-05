8499a884649101aeba0ae0e412d158979591272f
Tristan Webb <twebb@d-matrix.ai>
2022-08-05 09:17:12 -0700

    Remove fx transform function from file

--- src/mltools/corsair/transform.py
+++ src/mltools/corsair/transform.py
@@ -18,7 +18,6 @@ from sol.src.sol_sim import *
 from typing import Any, Dict, Iterator, List, Optional, Tuple, Union
 import torch.nn as nn
 from ..numerical import CastTo
-from ..fx import QuantTracer, InputOutputTransformer
 from torch.fx import GraphModule
 import warnings
 
@@ -41,32 +40,6 @@ def aware():
     torch.nn.ReLU6 = ReLU6
     torch.nn.Tanh = Tanh
     torch.nn.GELU = GELU
-
-
-def cast_input_output_transform(
-    root: torch.nn.Module,
-    input_fn: nn.Module = CastTo(),
-    output_fn: nn.Module = CastTo(),
-    weight_fn: nn.Module = CastTo(),
-    concrete_args: Optional[Dict[str, Any]] = None,
-) -> nn.Module:
-    tracer = QuantTracer()
-    graph = tracer.trace(root, concrete_args)
-    name = (
-        root.__class__.__name__ if isinstance(root, torch.nn.Module) else root.__name__
-    )
-    gm = GraphModule(tracer.root, graph, name)
-    cast_module_names = ["input_cast", "output_cast", "weight_cast"]
-    cast_module_functions = [input_fn, output_fn, weight_fn]
-
-    for mn, mf in zip(cast_module_names, cast_module_functions):
-        add_successful = gm.add_submodule(mn, mf)
-        if not add_successful:
-            warnings.warn("Error adding modue {}".format(mn), Warning)
-
-    transformed = InputOutputTransformer(gm).transform()
-
-    return transformed
 
 
 class Model(torch.nn.Module):
