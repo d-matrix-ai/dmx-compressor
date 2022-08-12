e1e404a7d957d2339bbe7eb784531bef4f24db08
Zifei Xu <xuzifei@d-matrix.ai>
2022-08-12 11:06:41 -0700

    uncommented corsair aware

--- src/mltools/corsair/transform.py
+++ src/mltools/corsair/transform.py
@@ -21,24 +21,23 @@ from ..numerical import CastTo
 
 
 def aware():
-    # # add new torch.nn modules for corsair
-    # torch.nn.CastTo = CastTo
-    # torch.nn.Sparsify = Sparsify
-    # torch.nn.Approximate = Approximate
-    # # overload existing torch.nn modules for corsair
-    # torch.nn.Linear = Linear
-    # torch.nn.Conv2d = Conv2d
-    # torch.nn.AdaptiveAvgPool2d = AdaptiveAvgPool2d
-    # torch.nn.MaxPool2d = MaxPool2d
-    # torch.nn.BatchNorm2d = BatchNorm2d
-    # torch.nn.LayerNorm = LayerNorm
-    # torch.nn.Dropout = Dropout
-    # torch.nn.Softmax = Softmax
-    # torch.nn.ReLU = ReLU
-    # torch.nn.ReLU6 = ReLU6
-    # torch.nn.Tanh = Tanh
-    # torch.nn.GELU = GELU
-    pass
+    # add new torch.nn modules for corsair
+    torch.nn.CastTo = CastTo
+    torch.nn.Sparsify = Sparsify
+    torch.nn.Approximate = Approximate
+    # overload existing torch.nn modules for corsair
+    torch.nn.Linear = Linear
+    torch.nn.Conv2d = Conv2d
+    torch.nn.AdaptiveAvgPool2d = AdaptiveAvgPool2d
+    torch.nn.MaxPool2d = MaxPool2d
+    torch.nn.BatchNorm2d = BatchNorm2d
+    torch.nn.LayerNorm = LayerNorm
+    torch.nn.Dropout = Dropout
+    torch.nn.Softmax = Softmax
+    torch.nn.ReLU = ReLU
+    torch.nn.ReLU6 = ReLU6
+    torch.nn.Tanh = Tanh
+    torch.nn.GELU = GELU
 
 
 class Model(torch.nn.Module):
