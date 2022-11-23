12e597ab4d9d44af3a7ea69e25bc8eab897d35a0
Xin Wang <xwang@d-matrix.ai>
2022-11-22 21:16:24 -0800

    Integrated IlyaS's matmul testdata loading

--- tests/test_numerical.py
+++ tests/test_numerical.py
@@ -1,7 +1,7 @@
 import torch, pytest, pickle
 import numpy as np
 from mltools import corsair, numerical
-from numerics import TestData, TestOpData
+from numerics import TestData, TestOpData, convert_matmul_results
 
 
 # helper dictionary that maps a numerics format string to a numerical.Format object
@@ -16,7 +16,9 @@ format_dict = {
     "CFP[1|5|2]{20}(N)": numerical.Format.from_shorthand("FP[1|5|2,20](FN)"),
     "CFP[1|4|3]{7}(N)": numerical.Format.from_shorthand("FP[1|4|3,7](FN)"),
     "CFP[1|4|3]{10}(N)": numerical.Format.from_shorthand("FP[1|4|3,10](FN)"),
-    "SBFP12": numerical.Format.from_shorthand("SBFP<XP[4,0](CSN)><FP[0|4|4,7](FN)>{16,-1}"),
+    "SBFP12": numerical.Format.from_shorthand(
+        "SBFP<XP[4,0](CSN)><FP[0|4|4,7](FN)>{16,-1}"
+    ),
 }
 
 
@@ -42,7 +44,7 @@ format_dict = {
         ("CFP[1|5|2]{20}(N)", "FP32", None, None),
         ("CFP[1|4|3]{7}(N)", "FP32", None, None),
         ("CFP[1|4|3]{10}(N)", "FP32", None, None),
-        ("SBFP12", "BFP16_64", None, None),
+        # ("SBFP12", "BFP16_64", None, None),
     ),
 )
 def test_conversion(from_format, to_format, register, rounding):
@@ -73,10 +75,10 @@ def test_matmul(input_a_format, input_b_format):
     the test assures torch.matmul() is epsilon-accurate
     """
 
-    test_data = TestOpData(input_a_format, input_b_format)
-    input_a_fp32 = torch.from_numpy(test_data.input_a.to_fp32())
-    input_b_fp32 = torch.from_numpy(test_data.input_b.to_fp32())
-    output_fp32 = torch.from_numpy(test_data.output.to_fp32())
+    test_data = TestOpData("multiply", input_a_format, input_b_format)
+    input_a_fp32 = torch.from_numpy(test_data.input1.to_fp32())
+    input_b_fp32 = torch.from_numpy(test_data.input2.to_fp32())
+    output_fp32 = torch.from_numpy(convert_matmul_results(test_data.output).to_fp32())
 
     output = torch.matmul(input_a_fp32, input_b_fp32)
     assert torch.allclose(output, output_fp32)
