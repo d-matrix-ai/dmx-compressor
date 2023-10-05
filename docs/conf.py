bc21f13d05ee31e5469034000ce2091eb543d9fa
Tristan Webb <twebb@d-matrix.ai>
2023-10-05 15:52:53 -0700

    Add read the docs autodoc support

--- docs/conf.py
+++ docs/conf.py
@@ -27,10 +27,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
 html_static_path = ['_static']
 
 
 import pathlib
 import sys
+import os
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
+sys.path.insert(0, os.path.abspath('../../dmx-mltools'))
