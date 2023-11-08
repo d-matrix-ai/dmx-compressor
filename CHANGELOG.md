3e0e62cdae62a58e82ecb0195a2da6d4f78fe2ee
Tristan Webb <twebb@d-matrix.ai>
2023-11-08 15:41:25 -0800

    bump version to 0.1.2 (#378)
    * bump version to 0.1.2
    
    * add diffusers

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,13 +1,20 @@
 # Changelog
 
-This file documents fixes/features added mltools
+This file documents fixes/features added to mltools
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## Unreleased (2023-06-16)
+## [Unreleased]
+
+## [v0.1.2] (2023-11-08)
+
 - Removed HuggingFace modules form DMX aware
+- Added SmoothQuant quantization techniques
+- Added Optimal Brain Compression activation calibration
+- FX Transformation redesign
+- Support for HuggingFace pipelines
 
 ## [v0.1.1] (2023-02-01)
-- Internal version
 
+- Internal version
--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import os
 from setuptools import find_packages, setup
 
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 
 setup(
     name="mltools",
     version=VERSION,
     description="d-Matrix ML tools",
-    author="Xin Wang, Michael Klachko, Tristan Webb, Sayeh Sharify, Wanzin Yazar",
-    author_email="xwang@d-matrix.ai, mklachko@d-matrix.ai, twebb@d-matrix.ai, sayehs@d-matrix.ai, wyazar@d-matrix.ai",
-    license="MIT",
+    author="Xin Wang, Michael Klachko, Tristan Webb, Sayeh Sharify, Wanzin Yazar, Zifei Xu",
+    author_email="xwang@d-matrix.ai, mklachko@d-matrix.ai, twebb@d-matrix.ai, sayehs@d-matrix.ai, wyazar@d-matrix.ai, xuzifei@d-matrix.ai",
+    license="Apache 2.0",
     packages=find_packages("src", exclude=("tests", "docs", "experiments", "sandbox")),
     package_dir={"": "src"},
     package_data={"configs": ["*.yaml"]},
