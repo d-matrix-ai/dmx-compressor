38a203f21be7cf8fa9e6c0b5816534b707d58c57
Tristan Webb <twebb@d-matrix.ai>
2024-09-11 15:52:43 -0400

    Add default error msg

--- src/dmx/compressor/quant/__init__.py
+++ src/dmx/compressor/quant/__init__.py
@@ -1,10 +1,7 @@
 try:
     from .quant_function import *
 except ImportError as error:
-    print(error.__class__.__name__ + ": " + error.message)
-except Exception as exception:
-    print(exception, False)
-    print(exception.__class__.__name__ + ": " + exception.message)
+    print("Error importing Block Quantize CUDA kernels")
 
 __all__ = [
     "fixed_point_quantize",
