dc11602536c226eb8babeba240b97bba2caa338b
Zifei Xu <xuzifei@d-matrix.ai>
2024-12-03 21:26:13 +0000

    fixing failing test cases

--- tests/test_additional_mappings.py
+++ tests/test_additional_mappings.py
@@ -7,13 +7,14 @@ from torch.library import custom_op
 
 from dmx.compressor.modeling import DmxModel, DmxConfigRule
 from dmx.compressor.modeling import nn as dmxnn
-from dmx.compressor.numerical import  CastTo, CastToDict
+from dmx.compressor.numerical import CastTo, CastToDict
 
 
 @custom_op("mylib::my_func", mutates_args=())
 def my_func(x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-    out = x+y
+    out = x + y
     return out
+
 
 class DmxSubmod(dmxnn.DmxModule):
     def __init__(self) -> None:
@@ -23,31 +24,33 @@ class DmxSubmod(dmxnn.DmxModule):
         )
 
     def _forward(self, x, y):
-        out = x+y
+        out = x + y
         return out
+
 
 class Model(torch.nn.Module):
     def __init__(self):
         super().__init__()
 
     def forward(self, x):
-        out = my_func(x,x)
+        out = my_func(x, x)
         return out
 
-def test_additional_mappings():
-   model = Model()
-   x = torch.ones((1,1))
-   additional_mappings = {"torch.ops.mylib.my_func.default": DmxSubmod}
-   dmx_model = DmxModel.from_torch(model, additional_mappings)
-   dmx_model(x)
 
-   assert isinstance(dmx_model._gm.my_func_default, DmxSubmod)
+def test_additional_mappings():
+    model = Model()
+    x = torch.ones((1, 1))
+    additional_mappings = {"torch.ops.mylib.my_func.default": DmxSubmod}
+    dmx_model = DmxModel.from_torch(model, additional_mappings)
+    dmx_model(x)
+
+    assert isinstance(dmx_model._gm.my_func_default, DmxSubmod)
+
 
 def test_no_additional_mappings():
-   model = Model()
-   x = torch.ones((1,1))
-   dmx_model = DmxModel.from_torch(model)
-   dmx_model(x)
+    model = Model()
+    x = torch.ones((1, 1))
+    dmx_model = DmxModel.from_torch(model)
+    dmx_model(x)
 
-   dmx_model._gm.graph.print_tabular()
-   assert hasattr(dmx_model._gm, "my_func_default") == False
+    assert hasattr(dmx_model._gm, "my_func_default") == False
