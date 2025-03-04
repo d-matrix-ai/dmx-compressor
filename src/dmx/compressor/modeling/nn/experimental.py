002192c942aa5b9e335db5885ab93a2fdf3641c4
sayehs-dmatrix <113129656+sayehs-dmatrix@users.noreply.github.com>
2025-03-04 18:15:48 -0500

    nn.experimental.Conv2d implementation with matmul (#787)

--- src/dmx/compressor/modeling/nn/experimental.py
+++ src/dmx/compressor/modeling/nn/experimental.py
@@ -171,3 +171,89 @@ class Conv1d(DmxModule, torch.nn.Conv1d):
             )
             g.output(_output_dq)
         return g
+
+class Conv2d(DmxModule, torch.nn.Conv2d):
+    r"""
+    This is an alternative version of the DmxModule.nn.Conv2d,
+    without calling torch.nn.functional.conv2d(), but torch.nn.functional.unfold() and torch.matmul() instead.
+    """
+
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        padding=0,
+        dilation=1,
+        groups=1,
+        bias=True,
+        padding_mode="zeros",
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+            dilation=dilation,
+            groups=groups,
+            bias=bias,
+            padding_mode=padding_mode,
+            **kwargs,
+        )
+        self.input_casts.input_cast.block_dim = 1
+        self.weight_cast.block_dim = 1
+        if self.bias_cast is not None:
+            self.bias_cast.block_dim = -1
+
+    def forward(self, _input: torch.Tensor) -> torch.Tensor:
+        _, _, in_height, in_width = _input.shape
+        _h_out = (in_height + 2 * self.padding[0] - (self.kernel_size[0] - 1) - 1) // self.stride[0] + 1
+        _w_out = (in_width + 2 * self.padding[1] - (self.kernel_size[1] - 1) - 1) // self.stride[1] + 1
+        _weight = self._weight.reshape((self.out_channels, -1))
+        _input = torch.nn.functional.unfold(
+            _input,
+            kernel_size=self.kernel_size,
+            dilation=self.dilation,
+            padding=self.padding,
+            stride=self.stride,
+        )
+        _convolution = self.accum_cast(torch.matmul(_weight, _input))
+        if self.bias is not None:
+            _output = torch.add(_convolution, self._bias.unsqueeze(-1))
+        else:
+            _output = _convolution
+        return torch.nn.functional.fold(_output, (_h_out, _w_out), (1, 1))
+
+    @staticmethod
+    def from_raw(raw: torch.nn.Module) -> DmxModule:
+        """
+        Creates a new Conv2d object (DmxModule) from a given PyTorch Conv2d layer.
+
+        Args:
+            raw (torch.nn.Module): A PyTorch Conv2d layer to be converted.
+
+        Returns:
+            DmxModule: A Conv2d object that has the same configuration as the input PyTorch Conv2d layer.
+        """
+        initial_dmx = Conv2d(
+            raw.in_channels,
+            raw.out_channels,
+            raw.kernel_size,
+            stride=raw.stride,
+            padding=raw.padding,
+            dilation=raw.dilation,
+            groups=raw.groups,
+            bias=raw.bias is not None,
+            padding_mode=raw.padding_mode,
+        )
+        initial_dmx.update_params_with_raw(raw)
+        return initial_dmx
+
+    def to_compiler_graph(self) -> Graph:
+        """
+        Returns a compiler friendly graph
+        """
+        raise NotImplementedError("to_compiler_graph not implemented!")
