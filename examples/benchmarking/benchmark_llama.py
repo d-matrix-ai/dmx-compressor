c5b2d2340ceda799f05867b49f1d88c4cea9e8d7
hmostafa <hmostafa@d-matrix.com>
2025-05-01 17:11:57 +0000

    added benchmarking utilties and scripts for CLIP and Llama

---
+++ examples/benchmarking/benchmark_llama.py
@@ -0,0 +1,44 @@
+import torch
+from transformers import CLIPProcessor, CLIPModel
+from transformers import pipeline
+
+from benchmark import measure_model_runtime,measure_model_error,EVALUATION_MODE
+    
+    
+def llama_model_maker():
+    device = torch.device('cuda')
+
+    model = "d-matrix/Llama-3.2-1b"
+    task = "text-generation"
+    pipe = pipeline(
+        task=task,
+        model=model,
+        trust_remote_code=True,
+        device_map="auto",
+    )
+    
+    def model_runner(m):
+        return pipe(text_inputs="The unanimous Declaration of the thirteen united States of America, ", max_new_tokens=1)
+
+    return [pipe.model,model_runner,None,device]
+
+
+def main():
+    active_modes = [EVALUATION_MODE.BASIC,EVALUATION_MODE.BASELINE]
+
+    print('********RUNTIME measurments of Llama')
+    measure_model_runtime(llama_model_maker,active_modes)
+    
+    print('********Error analysis Llama')
+    measure_model_error(llama_model_maker,active_modes,EVALUATION_MODE.BASELINE)
+
+
+
+
+
+    
+    
+
+    
+if __name__ == '__main__':
+    main()
